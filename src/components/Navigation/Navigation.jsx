5e991b591eed54f58661bc51b51fb96a53932ff0
LieraBerh <lierrastern@gmail.com>
2024-07-28 11:26:46 +0200

    upd

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,6 +1,7 @@
 import { NavLink } from "react-router-dom";
 import clsx from "clsx";
 import s from "./Navigation.module.css";
+import { GiRaceCar } from "react-icons/gi";
 
 const buildLinkClass = ({ isActive }) => {
   return clsx(s.link, isActive && s.active);
@@ -9,7 +10,10 @@ const buildLinkClass = ({ isActive }) => {
 const Navigation = () => {
   return (
     <header className={s.header}>
-      <nav>
+      <NavLink to="/" className={buildLinkClass}>
+        <GiRaceCar size={40} style={{ fill: "white" }} />
+      </NavLink>
+      <nav className={s.nav}>
         <ul className={s.list}>
           <li>
             <NavLink to="/" className={buildLinkClass}>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,37 @@
-import { useLocation } from "react-router-dom";
+/* eslint-disable react/no-unescaped-entities */
+import { Link } from "react-router-dom";
+import s from "./HomePage.module.css";
 
 const HomePage = () => {
-  const location = useLocation();
-  return <div>Home Page</div>;
+  return (
+    <div className={s.hero}>
+      <div className={s.title_container}>
+        <h1 className={s.hero_title}>
+          Find, book and rent a car <span className={s.accent}>Easily</span>
+        </h1>
+        <svg
+          width="175"
+          height="40"
+          viewBox="0 0 135 20"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className={s.svg}
+        >
+          <path
+            d="M134.398 2.44549C57.1256 -2.5909 23.8505 5.91274 1.51269 9.4328C1.16554 12.3701 1.29738 14.7861 0.927661 19.4528C60.8229 -0.00283289 100.753 3.19444 134.353 4.43383C134.367 4.06436 134.357 3.5539 134.398 2.44549Z"
+            fill="#1572D3"
+          />
+        </svg>
+      </div>
+      <p className={s.hero_text}>
+        Choose from a wide range of vehicles and book your perfect ride in
+        minutes. Whether for a weekend getaway or daily commuting, we've got you
+        covered. Start your journey with us today!
+      </p>
+      <Link to="/catalogue" className={s.link}>
+        Rent a car now
+      </Link>
+    </div>
+  );
 };
 export default HomePage;
